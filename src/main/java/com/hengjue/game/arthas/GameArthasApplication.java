30db28fda32931419e9c36e84a384b28445b0145
cein <1152260784@qq.com>
2020-07-14 17:36:28 +0800

    Initial commit

---
+++ src/main/java/com/hengjue/game/arthas/GameArthasApplication.java
@@ -0,0 +1,13 @@
+package com.hengjue.game.arthas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameArthasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameArthasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hengjue/game/arthas/GameArthasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hengjue.game.arthas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameArthasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
